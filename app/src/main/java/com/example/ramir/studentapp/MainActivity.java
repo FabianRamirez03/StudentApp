17e531a72afb452a4985203131ebebdb677f8ca1
Jessica <jessicaesp2000@gmail.com>
2018-11-22 20:26:54 -0600

    Can now switch between activities Created new class Sprite

--- app/src/main/java/com/example/ramir/studentapp/MainActivity.java
+++ app/src/main/java/com/example/ramir/studentapp/MainActivity.java
@@ -1,13 +1,35 @@
 package com.example.ramir.studentapp;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private View view;
+    private Button loginBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        loginBtn = findViewById(R.id.loginBtn);
+        loginBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openAppActivity();
+            }
+        });
     }
+
+    public void openAppActivity(){
+        Intent intent = new Intent(this, AppActivity.class);
+        startActivity(intent);
+    }
+
+
+
 }
---
+++ app/src/main/java/org/studentApp/sprites/Sprite.java
@@ -0,0 +1,4 @@
+package org.studentApp.sprites;
+
+public class Sprite {
+}
